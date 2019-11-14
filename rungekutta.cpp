113664681a5a761101eaadc036bc0f4c5d6a4dbf
diego moreno <da.moreno17@uniandes.edu.co>
2019-11-14 18:15:59 +0000

    rungekutta de 4to orden correccion

--- rungekutta.cpp
+++ rungekutta.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 float rungek4(float x_i, float t_i, float (*F)(float , float)){ 
     
-    float h = 0.01;    
-    float k1 = h*F(t_i, x_i);    
-    float k2 = h*F(t_i+h/2 , x_i+k1/2);
-    float k3 = h*F(t_i+h/2, x_i+k2/2 );
-    float k4 = h*F(t_i+h/2 , x_i+k3);    
+    float dt = 0.01;    
+    float k1 = dt*F(t_i, x_i);    
+    float k2 = dt*F(t_i+dt/2 , x_i+k1/2);
+    float k3 = dt*F(t_i+dt/2, x_i+k2/2 );
+    float k4 = dt*F(t_i+dt/2 , x_i+k3);    
     
     float y = x_i + 1.0/6.0*(k1 + 2*k2 + 2*k3 + k4);
     
@@ -27,14 +27,14 @@ float funcion (float t, float x ){
     
 int main(){
     
-    float h = 0.01;
+    float dt = 0.01;
     float xnow = 1.0;  
     float xnew = 1.0;  
     float t = 0.0;
     
     while (t<20){
         xnew = rungek4(xnow, t, funcion);
-        t += h;
+        t += dt;
         cout<<t<<"\t"<<xnew<<endl;
         xnow = xnew;
        }       
